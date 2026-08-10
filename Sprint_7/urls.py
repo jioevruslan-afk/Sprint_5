e47c7128b82e3454fea333565b77446f50f7f487
Ruslan <hvalii@yandex.ru>
2026-08-10 19:14:10 +0300

    Revert "Add Sprint 7 API tests"
    This reverts commit 674fea0e03782302f311b749a394f2e3aa94af0d.

--- Sprint_7/urls.py
+++
@@ -1,8 +0,0 @@
-BASE_URL = "https://qa-scooter.praktikum-services.ru/api/v1"
-
-COURIER_URL = f"{BASE_URL}/courier"
-COURIER_LOGIN_URL = f"{COURIER_URL}/login"
-ORDERS_URL = f"{BASE_URL}/orders"
-ACCEPT_ORDER_URL = f"{ORDERS_URL}/accept"
-CANCEL_ORDER_URL = f"{ORDERS_URL}/cancel"
-ORDER_BY_TRACK_URL = f"{ORDERS_URL}/track"
